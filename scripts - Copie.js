e7fa47805bf0d13336a6bfaba9b3a368f059c88e
Jeanc2 <91769202+Jeanc2@users.noreply.github.com>
2021-10-01 23:55:23 -0400

    Add files via upload

---
+++ scripts - Copie.js
@@ -0,0 +1,40 @@
+var elv_prices = new Array();
+elv_prices["standard"]=7565;
+elv_prices["premium"]=12.345;
+elv_prices["excelium"]=15.400;
+
+function getPlsPrice() {
+
+}
+
+
+
+
+
+function calculateResidentialPrice() {
+		console.log('resultsInputs');
+		let numberOpfColumn = 1;
+		let nbAppR = document.getElementById("number-of-apartments").value;
+		let nbFloorR = document.getElementById("number-floor").value;
+		let moyenneEleR = nbAppR/nbFloorR;
+		console.log('moyenneEleR',moyenneEleR);
+		let numberOfElevators = moyenneEleR/6;
+		if (numberOfElevators < 1) {
+			numberOfElevators = 1;
+	
+	else {
+		numberOfElevators = Math.floor(numberOfElevators);
+	
+
+	let numberOfElevators =nbFloorR/20;
+
+	console.log("numberOpfColumn",numberOpfColumn);
+	console.log("numberOfElevators",elevator-amount)
+	document.getElementById("elevator-amount").value = moyenneEleR
+
+}
+
+function calculateComercialPrice() {
+	
+letnbElevC = document.getElementById(C-number-of-elevators).value;
+
